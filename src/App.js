19bc952f67cee2c299ddb915a32af3b676679a5a
Khanh Le <khanh.le@wizeline.com>
2019-08-02 17:55:13 +0700

    Update App and App.test

--- src/App.js
+++ src/App.js
@@ -5,18 +5,37 @@ import requireContext from "require-context.macro";
 const images = requireContext("./images");
 
 function App() {
-  const drink = "lemonade";
+  const drink = process.env.REACT_APP_DRINK;
 
   return (
     <div className="App">
       <header className={`App-header ${drink}`}>
-        <p>
-          Hey, nice to meet you! Have some <i>{drink}!</i>
-        </p>
-        <img src={images(`./${drink}.jpg`)} alt="Refreshing drink" />
+        <Message drink={drink} />
+        <Image drink={drink} />
       </header>
     </div>
   );
+}
+
+export const Message = ({ drink }) => (
+  <p>
+    <i>
+      Hey, nice to meet you!
+      {validDrink(drink) ? ` Here is some special ${drink} for you!` : ""}
+    </i>
+  </p>
+);
+
+export const Image = ({ drink }) => {
+  if (validDrink(drink)) {
+    return <img src={images(`./${drink}.jpg`)} alt="Refreshing drink" />;
+  }
+
+  return null;
+};
+
+function validDrink(drink) {
+  return ["lemonade", "cocktail"].indexOf(drink) >= 0;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,44 @@
 import React from "react";
 import { shallow } from "enzyme";
-import App from "./App";
 
-it("renders without crashing", () => {
-  shallow(<App />);
+import App, { Message, Image } from "./App";
+
+describe("App", () => {
+  it("should render <Message /> and <Image /> components", () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find(Message).length).toEqual(1);
+    expect(wrapper.find(Image).length).toEqual(1);
+  });
 });
 
-it("should show a lemonade image", () => {
-  const app = shallow(<App />);
-  expect(app.find("img").prop("src")).toEqual("lemonade.jpg");
+describe("Message", () => {
+  it("should render the default message", () => {
+    const wrapper = shallow(<Message />);
+    expect(wrapper.text()).toEqual(`Hey, nice to meet you!`);
+  });
+
+  it("should render the message with drink", () => {
+    const mockDrink = "lemonade";
+    const wrapper = shallow(<Message drink={mockDrink} />);
+
+    expect(wrapper.text()).toEqual(
+      `Hey, nice to meet you! Here is some special lemonade for you!`
+    );
+  });
 });
 
-xit("should show a cocktail image", () => {
-  const app = shallow(<App />);
-  expect(app.find("img").prop("src")).toEqual("cocktail.jpg");
+describe("Image", () => {
+  it("by default, it shouldn't render any image", () => {
+    const wrapper = shallow(<Image />);
+
+    expect(wrapper.find("img").length).toEqual(0);
+  });
+
+  it("should render the correct drink image", () => {
+    const mockDrink = "lemonade";
+    const wrapper = shallow(<Image drink={mockDrink} />);
+
+    expect(wrapper.find("img").prop("src")).toEqual(`${mockDrink}.jpg`);
+  });
 });
